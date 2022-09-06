35de17cabc8980d81b9733c8656df370dcc4e709
Andrea Santurbano <santand@gmail.com>
2022-09-06 15:06:18 +0200

    removed Spark 2.4 module and bumped version to 4.2.0

--- spark-2.4/src/main/scala/org/neo4j/spark/streaming/Neo4jOffset24.scala
+++
@@ -1,7 +0,0 @@
-package org.neo4j.spark.streaming
-
-import org.apache.spark.sql.sources.v2.reader.streaming.Offset
-
-case class Neo4jOffset24(offset: Long) extends Offset {
-  override def json(): String = offset.toString
-}
