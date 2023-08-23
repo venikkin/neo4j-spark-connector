96fdae1dddfa6c71dca50add50695c333d256d49
Florent Biville <445792+fbiville@users.noreply.github.com>
2023-08-23 16:00:03 +0200

    Make build script switch artifact IDs (#517)
    * Make build script switch artifact IDs
    
    Dynamic artifact IDs are not well supported by Maven
    
    Fixes #516
    
    * Use script in Python CI
    
    * Temporary debug
    
    * Remove misinterpreted arg
    
    * Add extra debug
    
    Signed-off-by: Florent Biville <florent.biville@neo4j.com>
    
    * More debug
    
    Signed-off-by: Florent Biville <florent.biville@neo4j.com>
    
    * Fix version substitution
    
    * Remove debug thingies
    
    * Minor wording update
    
    ---------
    
    Signed-off-by: Florent Biville <florent.biville@neo4j.com>
    Co-authored-by: Andrea Santurbano <andrea.santurbano@larus-ba.it>

--- maven-release.sh
+++ maven-release.sh
@@ -3,7 +3,7 @@
 set -eEuxo pipefail
 
 if [[ $# -lt 2 ]] ; then
-    echo "Usage ./maven-release.sh <DEPLOY_OR_INSTALL> <SCALA-VERSION> [<ALT_DEPLOYMENT_REPOSITORY>]"
+    echo "Usage ./maven-release.sh <GOAL> <SCALA-VERSION> [<ALT_DEPLOYMENT_REPOSITORY>]"
     exit 1
 fi
 
@@ -17,9 +17,15 @@ exit_script() {
   kill -- -$$ # Sends SIGTERM to child/sub processes
 }
 
+mvn_evaluate() {
+  local expression
+  expression="${1}"
+  mvn help:evaluate -Dexpression="${expression}" --quiet -DforceStdout
+}
+
 trap exit_script SIGINT SIGTERM
 
-DEPLOY_INSTALL=$1
+GOAL=$1
 SCALA_VERSION=$2
 if [[ $# -eq 3 ]] ; then
   ALT_DEPLOYMENT_REPOSITORY="-DaltDeploymentRepository=$3"
@@ -32,6 +38,10 @@ case $(sed --help 2>&1) in
   *) sed_i () { sed -i '' "$@"; };;
 esac
 
+
+
+projectVersion=$(mvn_evaluate "project.version")
+
 # backup files
 cp pom.xml pom.xml.bak
 cp common/pom.xml common/pom.xml.bak
@@ -39,13 +49,25 @@ cp test-support/pom.xml test-support/pom.xml.bak
 cp spark-3/pom.xml spark-3/pom.xml.bak
 
 # replace pom files with target scala version
-sed_i "s/<artifactId>neo4j-connector-apache-spark<\/artifactId>/<artifactId>neo4j-connector-apache-spark_$SCALA_VERSION<\/artifactId>/" pom.xml
+sed_i "s/<artifactId>neo4j-connector-apache-spark_parent<\/artifactId>/<artifactId>neo4j-connector-apache-spark_${SCALA_VERSION}_parent<\/artifactId>/" pom.xml
 sed_i "s/<scala.binary.version \/>/<scala.binary.version>$SCALA_VERSION<\/scala.binary.version>/" pom.xml
-sed_i "s/<artifactId>neo4j-connector-apache-spark<\/artifactId>/<artifactId>neo4j-connector-apache-spark_$SCALA_VERSION<\/artifactId>/" "common/pom.xml"
-sed_i "s/<artifactId>neo4j-connector-apache-spark<\/artifactId>/<artifactId>neo4j-connector-apache-spark_$SCALA_VERSION<\/artifactId>/" "test-support/pom.xml"
-sed_i "s/<artifactId>neo4j-connector-apache-spark<\/artifactId>/<artifactId>neo4j-connector-apache-spark_$SCALA_VERSION<\/artifactId>/" "spark-3/pom.xml"
+
+sed_i "s/<artifactId>neo4j-connector-apache-spark_parent<\/artifactId>/<artifactId>neo4j-connector-apache-spark_${SCALA_VERSION}_parent<\/artifactId>/" "test-support/pom.xml"
+sed_i "s/<artifactId>neo4j-connector-apache-spark_test-support<\/artifactId>/<artifactId>neo4j-connector-apache-spark_${SCALA_VERSION}_test-support<\/artifactId>/" "test-support/pom.xml"
+
+sed_i "s/<artifactId>neo4j-connector-apache-spark_common<\/artifactId>/<artifactId>neo4j-connector-apache-spark_${SCALA_VERSION}_common<\/artifactId>/" "common/pom.xml"
+sed_i "s/<artifactId>neo4j-connector-apache-spark_parent<\/artifactId>/<artifactId>neo4j-connector-apache-spark_${SCALA_VERSION}_parent<\/artifactId>/" "common/pom.xml"
+sed_i "s/<artifactId>neo4j-connector-apache-spark_test-support<\/artifactId>/<artifactId>neo4j-connector-apache-spark_${SCALA_VERSION}_test-support<\/artifactId>/" "common/pom.xml"
+
+sed_i "s/<artifactId>neo4j-connector-apache-spark<\/artifactId>/<artifactId>neo4j-connector-apache-spark_${SCALA_VERSION}<\/artifactId>/" "spark-3/pom.xml"
+sed_i "s/<!-- <version>\${project.parent.version}<\/version> -->/<version>${projectVersion}_for_spark_3<\/version>/" "spark-3/pom.xml"
+sed_i "s/<artifactId>neo4j-connector-apache-spark_parent<\/artifactId>/<artifactId>neo4j-connector-apache-spark_${SCALA_VERSION}_parent<\/artifactId>/" "spark-3/pom.xml"
+sed_i "s/<artifactId>neo4j-connector-apache-spark_common<\/artifactId>/<artifactId>neo4j-connector-apache-spark_${SCALA_VERSION}_common<\/artifactId>/" "spark-3/pom.xml"
+sed_i "s/<artifactId>neo4j-connector-apache-spark_test-support<\/artifactId>/<artifactId>neo4j-connector-apache-spark_${SCALA_VERSION}_test-support<\/artifactId>/" "spark-3/pom.xml"
 
 # build
-mvn clean "${DEPLOY_INSTALL}" -Pscala-"${SCALA_VERSION}" -DskipTests ${ALT_DEPLOYMENT_REPOSITORY}
+mvn clean "${GOAL}" -Pscala-"${SCALA_VERSION}" -DskipTests ${ALT_DEPLOYMENT_REPOSITORY}
 
-exit_script
+if [ ! ${CI:-false} = true ]; then
+  exit_script
+fi
